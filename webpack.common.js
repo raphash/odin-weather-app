7c1c9a1bd8076cde42209583745183af5b3021da
raphash <175216103+raphash@users.noreply.github.com>
2025-01-28 20:16:33 -0300

    build: css-loader and style-loader to apply css

--- webpack.common.js
+++ webpack.common.js
@@ -1,20 +1,28 @@
-const path = require('path');
-const HtmlWebpackPlugin = require('html-webpack-plugin');
-const ESLintWebpackPlugin = require('eslint-webpack-plugin');
+const path = require("path");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+const ESLintWebpackPlugin = require("eslint-webpack-plugin");
 
 module.exports = {
-  entry: './src/index.js',
+  entry: "./src/index.js",
   plugins: [
     new HtmlWebpackPlugin({
-      template: './src/template.html',
+      template: "./src/template.html",
     }),
     new ESLintWebpackPlugin({
-      configType: 'flat'
+      configType: "flat",
     }),
   ],
+  module: {
+    rules: [
+      {
+        test: /\.css$/i,
+        use: ["style-loader", "css-loader"],
+      },
+    ],
+  },
   output: {
-    path: path.resolve(__dirname, 'dist'),
-    filename: 'script.js',
+    path: path.resolve(__dirname, "dist"),
+    filename: "script.js",
     clean: true,
   },
 };
