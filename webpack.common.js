08682f4e939847ff57ad426b250b680cf7b1b62b
raphash <175216103+raphash@users.noreply.github.com>
2025-01-27 21:23:24 -0300

    build: install eslint and configure basic use

--- webpack.common.js
+++ webpack.common.js
@@ -1,11 +1,15 @@
 const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
+const ESLintWebpackPlugin = require('eslint-webpack-plugin');
 
 module.exports = {
   entry: './src/index.js',
   plugins: [
     new HtmlWebpackPlugin({
       template: './src/template.html',
+    }),
+    new ESLintWebpackPlugin({
+      configType: 'flat'
     }),
   ],
   output: {
